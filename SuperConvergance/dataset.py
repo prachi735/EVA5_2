ea0bc65007724ddb8e10d29a173601f9fd2d2856
Prachi735 <Prachi735@gmail.com>
2021-02-25 17:47:53 +0530

    update model utils

--- SuperConvergance/dataset.py
+++ SuperConvergance/dataset.py
@@ -69,22 +69,3 @@ def get_dataloader(data, shuffle=True, batch_size=128, num_workers=4, pin_memory
     dataloader = torch.utils.data.DataLoader(data, ** dataloader_args)
 
     return dataloader
-
-
-# class_correct = list(0. for i in range(10))
-# class_total = list(0. for i in range(10))
-# with torch.no_grad():
-#     for data in testloader:
-#         images, labels = data
-#         outputs = net(images)
-#         _, predicted = torch.max(outputs, 1)
-#         c = (predicted == labels).squeeze()
-#         for i in range(4):
-#             label = labels[i]
-#             class_correct[label] += c[i].item()
-#             class_total[label] += 1
-
-
-# for i in range(10):
-#     print('Accuracy of %5s : %2d %%' % (
-#         classes[i], 100 * class_correct[i] / class_total[i]))
--- SuperConvergance/model_utils.py
+++ SuperConvergance/model_utils.py
@@ -5,24 +5,31 @@ from torchsummary import summary
 from torch.optim import lr_scheduler
 from model import ResNet_18
 
+
 def get_optimizer(model, lr=0.01,
                   momentum=0.9, weight_decay=5e-4):
 
-    return optim.SGD(model.parameters(), lr=0.01,momentum=momentum, weight_decay=weight_decay)
+    return optim.SGD(model.parameters(), lr=0.01, momentum=momentum, weight_decay=weight_decay)
 
 
-def get_scheduler(optimizer, lr_policy):
+def get_scheduler(optimizer, lr_policy, args):
     if lr_policy == 'step':
         scheduler = lr_scheduler.StepLR(
-            optimizer, gamma=0.1)
+            optimizer, gamma=args['gamma'])  # 0.1
     elif lr_policy == 'plateau':
         scheduler = lr_scheduler.ReduceLROnPlateau(
-            optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
+            optimizer, mode=args['mode'], factor=args['factor'], threshold=args['threshold'], patience=args['patience'])
+        # optimizer, mode='min', factor=0.2, threshold=0.01, patience=5)
     elif lr_policy == 'cosine':
-        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-    # else:
-    #     return NotImplementedError('learning rate policy [%s] is not implemented', opt.lr_policy)
+        scheduler = lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args['T_max'])  # 200
+    elif lr_policy == 'one_cylce':
+        scheduler = lr_scheduler.OneCycleLR(
+            optimizer, max_lr=args['max_lr'], steps_per_epoch=len(args['data_loader']), epochs=args['epochs'])
+    else:
+         return NotImplementedError('learning rate policy [%s] is not implemented', lr_policy)
     return scheduler
+
 
 def get_loss_function():
     return nn.CrossEntropyLoss()
@@ -32,11 +39,10 @@ def get_model_summary(model, input_size=(3, 32, 32)):
     return summary(model, input_size=input_size)
 
 
-def get_previous_model(model_path,device):
-    model =  ResNet_18()
+def get_previous_model(model_path, device):
+    model = ResNet_18()
     model.load_state_dict(torch.load(model_path))
     return model.to(device)
-
 
 
 def get_learning_rate():
